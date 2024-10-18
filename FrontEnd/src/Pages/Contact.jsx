e6d87dc35902c35b2315049b761ba6da4ee0ce61
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2024-10-18 11:36:49 +0530

    fe

--- FrontEnd/src/Pages/Contact.jsx
+++ FrontEnd/src/Pages/Contact.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='flex flex-col justify-center items-center px-60'>
+      <div className='flex flex-col'>
+        <h1 className="text-4xl font-bold text-blue-400 font-serif">Get In Touch With Us</h1>
+        {/* <h1 className="text-2xl  font-semibold mt-4 mb-2 text-center">Get In Touch</h1> */}
+      </div>
+
+        <div className="flex max-md:flex-col justify-around gap-10">
+          <div className='mt-5  lg:flex-col shadow-xl rounded-xl p-10 w-80'>
+            <h2>Please email us at:</h2>
+            <p className='border-2 bg-slate-100 text-blue-400 rounded-lg m-2 p-1'><a href="sample@gmail.com">sample@gmail.com</a></p>
+          </div>
+          <div className='mt-5  lg:flex-col shadow-xl rounded-xl p-10 w-80'>
+            <h2>Call us at:</h2>
+            <p className='border-2 bg-slate-100  text-blue-400  rounded-lg m-2 p-1'>Phone:1234567890</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='py-8'><main>
-    <section className="welcome  bg-gradient-to-r from-blue-300 to-red-300 text-black p-10 rounded-lg shadow-lg">
-<h1 className="text-4xl font-bold">Welcome to Recipe Finder!</h1>
-<p className="mt-2 text-lg text-center">Your one-stop destination for culinary exploration!</p>
-{/* <button className="mt-4 bg-white text-blue-500 px-4 py-2 rounded-lg shadow">Get Started</button> */}
-</section>
+    <>
+  <div className='py-8 flex w-full'>
 
-    </main></div>
+      <div className='flex flex-1 justify-center items-center'>
+        <h1 className="text-4xl font-bold text-blue-400 font-serif">Get In Touch With Us</h1>
+        {/* <h1 className="text-2xl  font-semibold mt-4 mb-2 text-center">Get In Touch</h1> */}
+      </div>
+
+
+      <div className="bg-gradient-to-r flex-1right-0 from-gray-500 to-transparent h-40 text-white p-10 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold">Welcome to Recipe Finder!</h1>
+        <p className="mt-2 text-lg text-center">Your one-stop destination for culinary exploration!</p>
+      </div>
+
+    </div>
+    </>
   )
 }
 
