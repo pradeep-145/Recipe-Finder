2ddca3041e4a2fa4f0ac26a8d139749f453a6408
pradeep sakthi 👋 <spradeepsakthi2004@gmail.com>
2024-10-24 14:21:38 +0530

    Refactor Firebase initialization in index.js and update API key in FrontEnd

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -89,7 +89,7 @@ function App() {
             <div>
               <Navbar logout={true}  />
             </div>
-            <div className='flex h-auto pt-28 bg-lime-100'>
+            <div className='flex h-[80vh] pt-28 overflow-y-scroll bg-lime-100'>
               <WishList />
             </div>
             <div className='  bottom-0'>
--- FrontEnd/src/Pages/WishList.jsx
+++ FrontEnd/src/Pages/WishList.jsx
@@ -1,28 +1,48 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import Lottie from 'react-lottie';
+import animationData from '../assets/loadingAnimation.json'; 
+
 const WishList = () => {
   const [wishlist,setWishlist]=useState([]);
+  const [loading,setLoading]=useState(false);
   const token=localStorage.getItem('token');
   useEffect(() => {
+    setLoading(true);
     // Fetch wishlist from backend
     axios.get('http://localhost:3000/wishlist',{
       headers:{
         'Authorization':`Bearer ${token}`
       }
     }).then(response => {
+      setLoading(false);
       console.log(response.data);
       setWishlist(response.data); 
     }
     ).catch(error => console.error(error.response.data));
   }
   , []);
-
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData, 
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
 
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className='flex flex-col items-center '>
       <h1 className="text-4xl font-bold">Your Wishlist</h1>
       <div className='flex flex-wrap justify-center items-center mt-4'>
-        {wishlist.length>0 ? (
+        { loading?(
+          <div className='flex justify-center items-center w-screen h-screen'>
+           
+          <Lottie options={defaultOptions} height={400} width={400} />
+        </div>
+        ):
+        wishlist.length > 0 ?
+        (
           wishlist.map((recipe, index) => (
             <div key={index} className='border border-gray-300 h-[400px] rounded-lg p-4 m-2 w-80'>
               <img src={recipe.strMealThumb} className='w-full rounded-lg' alt={recipe.strMeal} />
