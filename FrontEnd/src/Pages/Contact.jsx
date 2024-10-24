3e5717f1160ff002c5d69333f78e0310bf004a66
SAKTHIVAISHNAVI N <sakthivaishnavi12@gmail.com>
2024-10-24 21:10:08 +0530

    Refactor Contact page styles

--- FrontEnd/src/Pages/Contact.jsx
+++ FrontEnd/src/Pages/Contact.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='flex flex-col justify-center items-center w-screen mb-5'>
+      <div className='bg-[#f8f2df] p-10 rounded-3xl border-2 shadow-2xl'>
       <div className='flex flex-col mt-8'>
-        <h1 className="text-5xl font-bold text-[#3f7e65] font-serif bg-white p-6 rounded-xl">Get In Touch With Us</h1>
+        <h1 className="text-5xl font-bold text-[#3f7e65] font-serif text-center rounded-xl">Get In Touch With Us</h1>
       </div>
 
         <div className="flex max-md:flex-col justify-around gap-10 mt-5 text-xl">
@@ -17,6 +18,7 @@ const Contact = () => {
             <p className='border-2 bg-slate-100  text-blue-400  rounded-lg m-2 p-1'>Phone:1234567890</p>
           </div>
         </div>
+    </div>
     </div>
   )
 }
